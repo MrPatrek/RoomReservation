016cdfa073eaa021481b27a66b12667cefc5fbdc
MrPatrek <babenko.a2000@gmail.com>
2023-11-22 19:31:18 +0100

    service configuration

---
+++ RoomReservationServer/Extensions/ServiceExtensions.cs
@@ -0,0 +1,25 @@
+﻿namespace RoomReservationServer.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader());
+            });
+        }
+
+        public static void ConfigureIISIntegration(this IServiceCollection services)
+        {
+            // We are fine with the default options.
+            services.Configure<IISOptions>(options =>
+            {
+
+            });
+        }
+    }
+}
--- RoomReservationServer/Program.cs
+++ RoomReservationServer/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using RoomReservationServer.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
 
 builder.Services.AddControllers();
 
@@ -8,7 +14,22 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
+
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+
+// We call the UseCors method above the UseAuthorization method, as Microsoft recommends.
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
